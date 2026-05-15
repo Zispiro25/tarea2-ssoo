f672e58aa8a4281464fea04caa09dbc24a81e364
Daniel Aguirre <main.data06@gmail.com>
2026-05-15 19:51:31 -0400

    main

--- src/homer_memory/main.c
+++ src/homer_memory/main.c
@@ -1,14 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "../homer_memory_API/homer_memory_API.h"
 
 int main(int argc, char const *argv[]) {
-  // montar la memoria
+  //format_memory((char *)argv[1]);
+  mount_memory((char *)argv[1]);
 
-//printf("%s\n", argv[1]);
- mount_memory(argv[1]);
+  start_process(10, "ventas");
+  start_process(20, "reportes");
+  list_processes();
 
-list_processes();
-int slots = processes_slots();
-printf("li %d\n",slots);
-list_files(25);
+  int pcb_libres = processes_slots();
+  printf("PCB free slots: %d\n", pcb_libres);
+
+  int slots_arch = file_table_slots(10);
+  printf("File-table free slots (pid=10): %d\n", slots_arch);
+
+  homerFile* f = open_file(10, "dino.jpg", 'w'); 
+  write_file(f, "Memoria/Cmd/dino.jpg");
+
+  f = open_file(10, "dino.jpg", 'r'); 
+  read_file(f, "dino_copia.jpg");
+  close_file(f);
+
+  list_files(10);
+  frame_bitmap_status();
+
+  delete_file(10, "dino.jpg");
+  finish_process(10); 
+
+  int cerrados = clear_all_processes();
+  printf("Procesos cerrados por clear_all_processes(): %d\n", cerrados);
+
+  unmount_memory();
   return 0;
 }
